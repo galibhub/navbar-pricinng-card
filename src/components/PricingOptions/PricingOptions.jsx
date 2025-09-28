0ba37e8527b12884743666481d68092252d28d5f
IBRAHIM AHMED GALIB <ibrahimgalib00@gmail.com>
2025-09-28 12:22:31 +0600

    Added card section

--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React,{use} from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricing from '../DaisyPricing/DaisyPricing';
 
 const PricingOptions = ({pricingPromise}) => {
 
@@ -11,8 +12,12 @@ const PricingOptions = ({pricingPromise}) => {
 
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 container mx-auto">
              
-             {
+             {/* {
                 pricingData.map(pricing=> <PricingCard key={pricing.id}pricing={pricing}></PricingCard>)
+             } */}
+
+             {
+                pricingData.map(pricing=><DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>)
              }
 
             </div>
